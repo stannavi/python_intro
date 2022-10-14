31cb1727490c7522ab185eb61bbcf3dd9eebffeb
Stan_Navi <stanlevit@yandex.ru>
2022-10-14 18:55:16 +0300

    Lesson2 Task 2 complete

--- gb_seminars_hw_Borisovsky/sem2_hw.py
+++ gb_seminars_hw_Borisovsky/sem2_hw.py
@@ -8,15 +8,34 @@
 
 from fractions import Fraction
 
-a = Fraction(input("Введите число: "))
-digit_sum = 0
-while a > 0:
-    if a % 1 == 0:
-        digit_sum = digit_sum + a % 10
-        a = a // 10
+
+# a = Fraction(input("Введите число: "))
+# digit_sum = 0
+# while a > 0:
+#     if a % 1 == 0:
+#         digit_sum = digit_sum + a % 10
+#         a = a // 10
+#     else:
+#         while a % 1 != 0:
+#             a = a * 10
+#
+# print("Сумма цифр числа a: ", digit_sum)
+
+# Задача 2. Напишите программу, которая принимает на вход число N и выдает набор произведений
+# чисел от 1 до N.
+
+# *Пример:*
+# - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+def factorial(number, count = 1):
+    for i in range(1, number + 1):
+        count *= i
+    return count
+
+N = int(input("Введите число: "))
+print(f'Набор произведений чисел от 1 до {N} = ', end='')
+for i in range(1, N + 1):
+    if i == N:
+        print(f'{factorial(i)}')
     else:
-        while a % 1 != 0:
-            a = a * 10
-
-print("Сумма цифр числа a: ", digit_sum)
-
+        print(f'{factorial(i)}', end=', ')
