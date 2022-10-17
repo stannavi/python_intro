9dfe966b9762bac345c52e51c4a89d35cade2ac0
Stan_Navi <stanlevit@yandex.ru>
2022-10-17 19:59:16 +0300

    Lesson3 Task2 complete

--- gb_seminars_hw_Borisovsky/sem3_hw.py
+++ gb_seminars_hw_Borisovsky/sem3_hw.py
@@ -19,4 +19,29 @@ for i in range(list_size):
 print(list)
 print(sum)
 
+# Задача 2. Напишите программу, которая найдёт произведение
+# пар чисел списка. Парой считаем первый и последний элемент,
+# второй и предпоследний и т.д.
+
+# *Пример:*
+# - [2, 3, 4, 5, 6] => [12, 15, 16];
+# - [2, 3, 5, 6] => [12, 15]
+
+list_size = int(input("Введите размер списка: "))
+list = []
+list2 = []
+
+for i in range(list_size):
+    list.append(int(input("Введите число: ")))
+
+for i in range(len(list)):
+    while i < len(list) / 2 < list_size:
+        list_size -= 1
+        a = list[i] * list[list_size]
+        list2.append(a)
+        i += 1
+
+print(list)
+print(list2)
+
 
