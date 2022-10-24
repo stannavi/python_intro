10b309cce57ac87fa7c6c6f69beb063dba9a7d3e
Stan_Navi <stanlevit@yandex.ru>
2022-10-24 17:54:45 +0300

    Lesson4 Task2 complete

--- gb_seminars_hw_Borisovsky/sem4_hw.py
+++ gb_seminars_hw_Borisovsky/sem4_hw.py
@@ -15,3 +15,21 @@ while simple_mult <= n:
         simple_mult += 1
 
 print(list_simple_mult)
+
+
+# задача 2 . Задайте последовательность чисел.
+# Напишите программу, которая выведет список неповторяющихся
+# элементов исходной последовательности.
+
+numbers = [2, 3, 45, 4, 2, 1, 1, 10, 3, 2, 15, 1]
+
+def unique_numbers_list(numbers):
+    unique = []
+    for i in numbers:
+        if i in unique:
+            continue
+        else:
+            unique.append(i)
+    return unique
+
+print(unique_numbers_list(numbers))
