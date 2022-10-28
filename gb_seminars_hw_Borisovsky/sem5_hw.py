d4afe99b652f91d33a10f4fc981e0b2c701fa10a
Stan_Navi <stanlevit@yandex.ru>
2022-10-28 18:52:49 +0300

    Lesson5 Task5 complete

--- gb_seminars_hw_Borisovsky/sem5_hw.py
+++ gb_seminars_hw_Borisovsky/sem5_hw.py
@@ -2,50 +2,51 @@
 # (здесь только буквы).
 # Входные и выходные данные хранятся в отдельных текстовых файлах.
 
-# with open('file_encode.txt', 'w') as data:
-#     data.write('wwhhhhhhhhhhyrrrkwrsssoosppppppoooorrrrrr')
-#
-# with open('file_encode.txt', 'r') as data:
-#     string = data.readline()
-#
-# def rle_encode(decoded_string):
-#     encoded_string = ''
-#     count = 1
-#     char = decoded_string[0]
-#     for i in range(1, len(decoded_string)):
-#         if decoded_string[i] == char:
-#             count += 1
-#         else:
-#             encoded_string = encoded_string + str(count) + char
-#             char = decoded_string[i]
-#             count = 1
-#             encoded_string = encoded_string + str(count) + char
-#     return encoded_string
-#
-#
-# def rle_decode(encoded_string):
-#     decoded_string = ''
-#     char_amount = ''
-#     for i in range(len(encoded_string)):
-#         if encoded_string[i].isdigit():
-#             char_amount += encoded_string[i]
-#         else:
-#             decoded_string += encoded_string[i] * int(char_amount)
-#         char_amount = ''
-#     print(decoded_string)
-#
-#     return decoded_string
-#
-#
-# with open('file_encode.txt', 'r') as file:
-#     decoded_string = file.read()
-#
-# with open('file_decode.txt', 'w') as file:
-#     encoded_string = rle_encode(decoded_string)
-#     file.write(encoded_string)
-#
-# print('Decoded string: \t' + decoded_string)
-# print('Encoded string: \t' + rle_encode(decoded_string))
+with open('file_encode.txt', 'w') as data:
+    data.write('wwhhhhhhhhhhyrrrkwrsssoosppppppoooorrrrrr')
+
+with open('file_encode.txt', 'r') as data:
+    string = data.readline()
+
+def rle_encode(decoded_string):
+    encoded_string = ''
+    count = 1
+    char = decoded_string[0]
+    for i in range(1, len(decoded_string)):
+        if decoded_string[i] == char:
+            count += 1
+        else:
+            encoded_string = encoded_string + str(count) + char
+            char = decoded_string[i]
+            count = 1
+            encoded_string = encoded_string + str(count) + char
+    return encoded_string
+
+
+def rle_decode(encoded_string):
+    decoded_string = ''
+    char_amount = ''
+    for i in range(len(encoded_string)):
+        if encoded_string[i].isdigit():
+            char_amount += encoded_string[i]
+        else:
+            decoded_string += encoded_string[i] * int(char_amount)
+        char_amount = ''
+    print(decoded_string)
+
+    return decoded_string
+
+
+with open('file_encode.txt', 'r') as file:
+    decoded_string = file.read()
+
+with open('file_decode.txt', 'w') as file:
+    encoded_string = rle_encode(decoded_string)
+    file.write(encoded_string)
+
+print('Decoded string: \t' + decoded_string)
+print('Encoded string: \t' + rle_encode(decoded_string))
+
 
 # задача 3. Напишите программу, удаляющую из текста все слова, содержащие "абв".
 # Функции FIND и COUNT юзать нельзя.
@@ -55,3 +56,22 @@ print(f"Исходный текст: {txt}")
 find_txt = "абв"
 lst = [i for i in txt.split() if find_txt not in i]
 print(f'Результат: {" ".join(lst)}')
+
+
+
+# задача 5 необязательная Дан список чисел. Создайте список, в который попадают числа,
+# описывающие максимальную возрастающую последовательность. Порядок элементов менять нельзя.
+# *Пример:*
+# [1, 5, 2, 3, 4, 6, 1, 7] => [1,  7]
+#     [1, 5, 2, 3, 4,  1, 7, 8 , 15 , 1 ] => [1,  5]
+
+nums = [1, 5, 2, 3, 4,  1, 7, 8 , 15 , 1]
+
+def get_up(nums):
+    ups = []
+    for i in range(len(nums)):
+        if nums[i] == max(nums[:i+1:]) and nums[i] not in ups:
+            ups.append(nums[i])
+    return ups
+
+print(get_up(nums))
